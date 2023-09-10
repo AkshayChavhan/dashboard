d109b5d1e93d0858cb259aac73237a7183b12109
akshaychavhan <akshay@rtledgers.com>
2023-09-10 10:30:57 +0530

    Geography Screen Ui and backend completed

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProducts , getCustomers ,getTransactions } from "../controllers/client.js";
+import { getProducts , getCustomers ,getTransactions  ,getGeography } from "../controllers/client.js";
 const router = express.Router();
 
 router.get("/products" ,getProducts);
@@ -7,5 +7,7 @@ router.get("/products" ,getProducts);
 router.get("/customers" ,getCustomers);
 
 router.get("/transactions" ,getTransactions);
+
+router.get("/geography" ,getGeography);
 
 export default router ;
